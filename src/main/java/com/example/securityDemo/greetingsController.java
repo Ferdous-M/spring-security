2e8c4838b9e26417d2d625b51db7f6ee8cfaacb6
Ferdous-M <mahzabinferdous000@gmail.com>
2026-06-06 19:54:09 +0600

    role based authentication

--- src/main/java/com/example/securityDemo/greetingsController.java
+++ src/main/java/com/example/securityDemo/greetingsController.java
@@ -1,5 +1,6 @@
 package com.example.securityDemo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +10,17 @@ public class greetingsController {
     public String greet() {
         return "Hello, World!";
     }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String greetUser() {
+        return "Hello, user!";
+    }
+
+    @GetMapping("/admin")
+    public String greetAdmin() {
+        return "Hello, Admin!";
+    }
+
+
 }
