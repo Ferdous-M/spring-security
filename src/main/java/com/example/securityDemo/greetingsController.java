321f5f40f31ad3e69e5f96ec456f35d3551eaf04
Ferdous-M <mahzabinferdous000@gmail.com>
2026-06-17 16:31:40 +0600

    dto added

--- src/main/java/com/example/securityDemo/greetingsController.java
+++ src/main/java/com/example/securityDemo/greetingsController.java
@@ -1,6 +1,8 @@
 package com.example.securityDemo;
 
 import com.example.securityDemo.jwt.JwtUtils;
+import com.example.securityDemo.jwt.LoginRequest;
+import com.example.securityDemo.jwt.LoginResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
